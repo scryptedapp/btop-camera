2dc0bc2cb3382d260e05bdfb1cf9b8f31f6409c1
Brett Jia <dev.bjia56@gmail.com>
2024-07-16 10:08:39 -0400

    better process cleanup, configurable display dimensions

--- src/main.py
+++ src/main.py
@@ -3,16 +3,18 @@ import os
 import pathlib
 import platform
 import shutil
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, Tuple
+
+import psutil
 
 import scrypted_sdk
 from scrypted_sdk import ScryptedDeviceBase, VideoCamera, ResponseMediaStreamOptions, Settings, Setting, ScryptedInterface, ScryptedMimeTypes
 
 
-async def run_and_stream_output(cmd: str, env: Dict[str, str] = {}, on_stdout: Callable[[str], Any] = print, on_stderr: Callable[[str], Any] = print) -> None:
-    async def run():
-        p = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, env=dict(os.environ, **env))
+async def run_and_stream_output(cmd: str, env: Dict[str, str] = {}, on_stdout: Callable[[str], Any] = print, on_stderr: Callable[[str], Any] = print, return_pid: bool = False) -> Tuple[asyncio.Future, int] | None:
+    p = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, env=dict(os.environ, **env))
 
+    async def read_streams():
         async def stream_stdout():
             async for line in p.stdout:
                 on_stdout(line.decode('utf-8'))
@@ -22,11 +24,15 @@ async def run_and_stream_output(cmd: str, env: Dict[str, str] = {}, on_stdout: C
 
         await asyncio.gather(stream_stdout(), stream_stderr(), p.wait())
 
-    await run()
+    if return_pid:
+        return (asyncio.ensure_future(read_streams()), p.pid)
+    await read_streams()
 
 
 class BtopCamera(ScryptedDeviceBase, VideoCamera, Settings):
-    XAUTH = os.path.join(os.environ['SCRYPTED_PLUGIN_VOLUME'], 'files', 'Xauthority')
+    FILES = os.path.join(os.environ['SCRYPTED_PLUGIN_VOLUME'], 'files')
+    XAUTH = os.path.join(FILES, 'Xauthority')
+    PIDFILE = os.path.join(FILES, 'Xvfb.pid')
 
     def __init__(self, nativeId: str = None) -> None:
         super().__init__(nativeId)
@@ -49,41 +55,68 @@ class BtopCamera(ScryptedDeviceBase, VideoCamera, Settings):
                     needed.append('xvfb-run')
                 if shutil.which('xterm') is None:
                     needed.append('xterm')
+                    needed.append('xfonts-base')
                 if shutil.which('btop') is None:
                     needed.append('btop')
-                if shutil.which('killall') is None:
-                    needed.append('psmisc')
 
                 if needed:
-                    needed.append('xfonts-base')
                     needed.sort()
                     raise Exception(f"Please manually install the following and restart the plugin: {needed}")
 
-            pathlib.Path(os.path.dirname(BtopCamera.XAUTH)).mkdir(parents=True, exist_ok=True)
+            pid = self.read_pidfile()
+            if pid:
+                try:
+                    xvfb_run_process = psutil.Process(pid)
+                    for child in xvfb_run_process.children(recursive=True):
+                        if child.name() == 'Xvfb':
+                            child.terminate()
+                except:
+                    pass
+                shutil.rmtree(BtopCamera.FILES, ignore_errors=True)
 
-            await run_and_stream_output('killall Xvfb')
-        except Exception as e:
+            pathlib.Path(BtopCamera.FILES).mkdir(parents=True, exist_ok=True)
+        except:
             import traceback
             traceback.print_exc()
+            os._exit(1)
 
     async def init_stream(self) -> None:
         await self.dependencies_installed
 
         async def run_stream():
             while True:
-                await run_and_stream_output(f'xvfb-run -n 99 -s "-screen 0 1024x720x24" -f {BtopCamera.XAUTH} xterm -maximized -e btop')
+                fut, pid = await run_and_stream_output(f'xvfb-run -n {self.virtual_display_num} -s "-screen 0 {self.display_dimensions}x24" -f {BtopCamera.XAUTH} xterm -maximized -e btop', return_pid=True)
+
+                # write pid to file
+                with open(BtopCamera.PIDFILE, 'w') as f:
+                    f.write(str(pid))
+
+                await fut
 
         async def run_ffmpeg():
             while True:
-                await run_and_stream_output(f'ffmpeg -loglevel error -f x11grab -video_size 1024x720 -framerate 15 -draw_mouse 0 -i :99 -c:v libx264 -preset ultrafast -g 60 -an -dn -f flv -listen 1 rtmp://localhost:{self.rtmp_port}/stream', env={'XAUTHORITY': BtopCamera.XAUTH})
+                await run_and_stream_output(f'ffmpeg -loglevel error -f x11grab -framerate 15 -draw_mouse 0 -i :{self.virtual_display_num} -c:v libx264 -qp 0 -preset ultrafast -g 60 -an -dn -f flv -listen 1 rtmp://localhost:{self.rtmp_port}/stream', env={'XAUTHORITY': BtopCamera.XAUTH})
 
         asyncio.gather(run_stream(), run_ffmpeg())
+
+    def read_pidfile(self) -> int:
+        try:
+            with open(BtopCamera.PIDFILE) as f:
+                return int(f.read())
+        except:
+            return None
 
     @property
     def virtual_display_num(self) -> int:
         if self.storage:
             return self.storage.getItem('virtual_display_num') or 99
         return 99
+
+    @property
+    def display_dimensions(self) -> int:
+        if self.storage:
+            return self.storage.getItem('display_dimensions') or '1024x720'
+        return '1024x720'
 
     @property
     def rtmp_port(self) -> int:
@@ -93,6 +126,13 @@ class BtopCamera(ScryptedDeviceBase, VideoCamera, Settings):
 
     async def getSettings(self) -> list[Setting]:
         return [
+            {
+                "key": "display_dimensions",
+                "title": "Virtual Display Dimensions",
+                "description": "The X11 virtual display dimensions to use. Format: WIDTHxHEIGHT.",
+                "type": "string",
+                "value": self.display_dimensions,
+            },
             {
                 "key": "virtual_display_num",
                 "title": "Virtual Display Number",
@@ -110,8 +150,9 @@ class BtopCamera(ScryptedDeviceBase, VideoCamera, Settings):
         ]
 
     async def putSetting(self, key: str, value: str) -> None:
-        self.storage.setItem(key, int(value))
+        self.storage.setItem(key, value)
         await self.onDeviceEvent(ScryptedInterface.Settings.value, None)
+        print("Settings updated, will restart...")
         await scrypted_sdk.deviceManager.requestRestart()
 
     async def getVideoStreamOptions(self) -> list[ResponseMediaStreamOptions]:
